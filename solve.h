cd6ed021fd47c41c1c71c9d3a5db03c4002f8c52
4465 <lwfrekcah@gmail.com>
2018-01-08 12:40:02 +0800

    1

---
+++ solve.h
@@ -0,0 +1,22 @@
+#ifndef GRAPHAPP_H
+#define GRAPHAPP_H
+#include "Graphmtx.h"
+#include "Graph.h"
+#include <limits.h>
+const int maxValue = INT_MAX;
+
+
+
+void printNext(Graph& G, int v, int dist[], int path[])   //Êä³öÏÂÒ»Ìø
+{
+	
+}
+
+
+
+void ShortestPath(Graph& G, int v, int dist[], int path[])
+{
+	
+}
+
+#endif
